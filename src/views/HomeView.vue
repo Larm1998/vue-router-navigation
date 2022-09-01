4a8689811c599a2c5edab34df18fa36083f441f6
Luis Rangel <luis.rangel@intechideas.com>
2022-09-01 17:01:52 -0500

    routes with names

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,11 @@ export default {
     return {
       username: '',
     }
+  },
+  methods: {
+    begin() {
+      this.$router.push('/chats')
+    }
   }
 }
 </script>
@@ -12,5 +17,6 @@ export default {
   <main>
     <h3>Enter your username {{ username }}</h3>
     <input placeholder="username" v-model="username"/>
+    <button @click="begin">Login</button>
   </main>
 </template>
