906f1467f2a30d02b9782494743d9e4a14bd551d
Luis Rangel <luis.rangel@intechideas.com>
2022-09-06 09:49:55 -0500

    Routes with names

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
 
 <template>
   <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'home' }">Home</router-link>
+    <router-link to="/session">Session</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link>
     <router-link to="/chats">Chats</router-link>
   </div>
   <router-view/>
